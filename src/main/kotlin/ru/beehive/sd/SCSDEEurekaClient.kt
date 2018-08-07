3daf483ed999eb57b0c2823892880bda50abccdc
D.Pchelintsev <d.pchelincev@virtusystems.ru>
2018-08-07 17:19:55 +0500

    1. добавил контроллер 2. мавеновский плагин для spring boot не хочет забирать проект без указания main class

--- src/main/kotlin/ru/beehive/sd/SCSDEEurekaClient.kt
+++ src/main/kotlin/ru/beehive/sd/SCSDEEurekaClient.kt
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @EnableEurekaClient
 @SpringBootApplication
-class SCSDE_EurekaClient {
+class SCSDEEurekaClient {
 
     fun main(args: Array<String>) {
-        SpringApplication.run(SCSDE_EurekaClient::class.java, *args)
+        SpringApplication.run(SCSDEEurekaClient::class.java, *args)
     }
 }
